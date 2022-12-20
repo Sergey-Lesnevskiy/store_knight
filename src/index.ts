f1017c91fb779d6cd750ce2dc9f786f128f9a394
adam malec <add.malec@gmail.com>
2022-12-20 11:29:33 +0300

    draft: init store page

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import './styles.css'
 import { products } from './json'
 import { ShoppingCart } from './components/shoppingCart/shoppingCart'
+import { PageHeader } from './components/pageHeader/pageHeader'
+import { CategoryFilter } from './components/categoryFilter/categoryFilter'
+import { Filters } from './components/filters/filters'
 
 const shopping  = new ShoppingCart();
 shopping.hi()
+
+const header = new PageHeader();
+header.hi();
+
+const categoryFilter = new CategoryFilter();
+categoryFilter.hi();
+
+const filters = new Filters();
+filters.hi();
