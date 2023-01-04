c77d71c470b81575a43b94abff4c9b84347741bd
adam malec <add.malec@gmail.com>
2023-01-04 23:24:06 +0300

    feat: add Error Page

--- src/pages/ErrorPage/ErrorPage.ts
+++ src/pages/ErrorPage/ErrorPage.ts
@@ -2,16 +2,21 @@ import { Page } from '../../core/templates/page';
 
 export class ErrorPage extends Page {
   private errorType: string;
-  static TextObject: { [prop: string]: string } = {
-    '404': 'Error! The page is not found',
-  };
+
   constructor(id: string, errorType: string) {
     super(id);
     this.errorType = errorType;
   }
   render() {
-    const title = ErrorPage.TextObject[this.errorType];
-    this.container.append(title);
+    this.container.innerHTML = `
+    <section class="error">
+      <div class="error__container">
+        <img class="error__image" src="./images/error-swords.svg" width="52" alt="">
+        <h1 class="error__title">404 Error</h1>
+        <p class="error__description">Page not found</p>
+      </div>
+    </section>
+    `;
     return this.container;
   }
 }
